208b942b712f80b0d93bc9ff3d9a702e90511e5d
damoqiongqiu <damoqiongqiu@126.com>
2019-04-13 11:06:18 +0800

    动画全部换成fadeIn

--- src/app/shared/animations/fade-in.ts
+++ src/app/shared/animations/fade-in.ts
@@ -3,9 +3,9 @@ import { trigger, style, transition, animate } from '@angular/animations';
 export const fadeIn = trigger('fadeIn', [
   transition("void => *", [
     style({ opacity: 0 }),
-    animate(600, style({ opacity: 1 }))
+    animate(300, style({ opacity: 1 }))
   ]),
   transition("* => void", [
-    animate(600, style({ opacity: 0 }))
+    animate(300, style({ opacity: 0 }))
   ])
 ]);
