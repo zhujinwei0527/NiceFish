9633f3f2b56c94d20bb47b3d380935fe2d835b40
damoqiongqiu <damoqiongqiu@126.com>
2019-08-20 15:20:38 +0800

    use-http-interceptor-as-baseurl

--- src/app/ApiEndpoints.ts
+++ src/app/ApiEndpoints.ts
@@ -3,7 +3,7 @@
  * @author 大漠穷秋
  */
 export class ApiEndpoints {
-    // public static API_ENDPOINT = "http://127.0.0.1:80/nicefish";
-    public static API_ENDPOINT = "http://localhost:4200";
+    public static API_ENDPOINT = "http://127.0.0.1:80/nicefish";
+    // public static API_ENDPOINT = "http://localhost:4200";
 }
 
--- src/app/ApiInterceptor.ts
+++ src/app/ApiInterceptor.ts
@@ -8,6 +8,11 @@ export class ApiInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(req.url);
         console.log(req.body);
+        // 不拦截assets和mock-data打头的请求
+        if (req.url.indexOf("assets") != -1
+            || req.url.indexOf("mock-data") != -1) {
+            return next.handle(req);
+        }
         const baseUrl = ApiEndpoints.API_ENDPOINT;
         const apiReq = req.clone({ url: `${baseUrl}${req.url}` });
         return next.handle(apiReq);
