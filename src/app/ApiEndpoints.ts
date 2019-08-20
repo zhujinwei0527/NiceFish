e782bc46b8ebba6e2b497ad8cab1edc6657d661f
damoqiongqiu <damoqiongqiu@126.com>
2019-08-20 16:00:26 +0800

    HttpInterceptor实现BaseURL

--- src/app/ApiEndpoints.ts
+++ src/app/ApiEndpoints.ts
@@ -4,6 +4,5 @@
  */
 export class ApiEndpoints {
     public static API_ENDPOINT = "http://127.0.0.1:80/nicefish";
-    // public static API_ENDPOINT = "http://localhost:4200";
 }
 
--- src/app/ApiInterceptor.ts
+++ src/app/ApiInterceptor.ts
@@ -8,7 +8,7 @@ export class ApiInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(req.url);
         console.log(req.body);
-        // 不拦截assets和mock-data打头的请求
+        // 不拦截assets和mock-data打头的请求，TODO:改成可配置的方式
         if (req.url.indexOf("assets") != -1
             || req.url.indexOf("mock-data") != -1) {
             return next.handle(req);
